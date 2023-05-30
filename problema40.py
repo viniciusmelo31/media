3ac1ed92ce8981855945f42d4510bd3a10d4c02a
vini <110433248+viniciusmelo31@users.noreply.github.com>
2023-05-30 14:09:26 -0300

    Add files via upload

---
+++ problema40.py
@@ -0,0 +1,14 @@
+def converter(graus):
+    return(g*3.14/(180))
+
+g = float(input("digite a temperatura em celsius: "))
+rad = converter(g)
+
+print("a temperatura em radianos é:", rad)
+
+
+
+
+
+
+
