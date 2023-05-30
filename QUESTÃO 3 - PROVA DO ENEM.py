7c4b25c032f4e0699790bb25682188ad2f9b0263
vini <110433248+viniciusmelo31@users.noreply.github.com>
2023-05-30 14:08:30 -0300

    Add files via upload

---
+++ QUESTÃO 3 - PROVA DO ENEM.py
@@ -0,0 +1,35 @@
+vnota = []
+vgabarito = []
+
+    
+for c in range(3):
+    nota = input("Digite o gabarito da questão" +str(c+1)+': ')
+    vgabarito.append(nota)
+
+nome = input("Digite o nome do(a) candidato(a): ")
+print("Nome do(a) candidato(a): ", nome)
+
+for c in range(3):
+    nota = input("Digite o gabarito da questão" +str(c+1)+': ')
+    vnota.append(nota)
+    
+
+
+
+
+
+
+
+
+
+    
+    
+    
+
+    
+    
+
+    
+    
+    
+    
