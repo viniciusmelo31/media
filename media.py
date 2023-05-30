7b267835611712934043d662b915de07a4b263d3
vini <110433248+viniciusmelo31@users.noreply.github.com>
2023-05-30 14:04:19 -0300

    Add files via upload

---
+++ media.py
@@ -0,0 +1,11 @@
+n1 = input('digite a nota: ')
+n1 = float(n1)
+n2 = input('digite outro: ')
+n2 = float(n2)
+soma = n1+n2
+soma = float(soma)
+media = soma /2
+media = float(media)
+print(' a media do aluno é', media)
+
+
