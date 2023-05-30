aad24eefceae553467581c9dec510ffa072e56a6
vini <110433248+viniciusmelo31@users.noreply.github.com>
2023-05-30 14:09:46 -0300

    Add files via upload

---
+++ 39.py
@@ -0,0 +1,25 @@
+def distanciapercorrida(dis, tempo):
+    return(dis/tempo)
+
+dis = float(input("digite a distancia percorrida: "))
+tempo = float(input("dite o tempo de viagem: "))
+
+d = distanciapercorrida(dis, tempo)
+
+print("A velocidade media é", d)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
